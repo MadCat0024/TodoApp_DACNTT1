ac2f3e035612360ecdbb1658b7053a87b98a969f
TranVu2582410 <trananhvu2016kg@gmail.com>
2023-05-27 17:19:31 +0700

    add func delete and update

--- App.js
+++ App.js
@@ -4,38 +4,63 @@ import React, {useEffect, useState} from 'react';
 import Task from './components/Task';
 import styles from './appComponents.Styles';
 import Form from './components/Form';
-import { getAll, insert} from './database/services'
+import { deleteTask, getAll, insert, queryAllTodoList} from './database/services'
 import { TODOLIST_SCHEMA} from './database/name'
+import { TodoListSchema } from './database/tables';
 //import { updateTodoList, deleteTodoList, queryAllTodoList, insertNewTodoList } from './database/allSchema'
 export default function App() {
   const [taskList, settaskList] = useState([])
-  console.log(taskList,'taskList')
+  //console.log(taskList,'taskList')
   useEffect(() => {
     handleAddTask()
   },[])
   const handleAddTask = async(task) => {
     try {
       let list = await getAll(TODOLIST_SCHEMA)
-      console.log(list)
+      // console.log(list)
       settaskList(list)
     } catch (error) {
       console.log(error)
     }
     
   }
-  const handleDeleteTask = (index) =>{
-    Alert.alert('Delete Task', 'Bạn đã hoàn thành việc này?', [
-      {
-        text: 'Yes',
-        onPress: () => {
-          let taskListTmp = [...taskList];
-          //xoá 1 phần tử từ vị trí index
-          taskListTmp.splice(index, 1);
-          settaskList(taskListTmp);
-        }
-      },
-      {text: 'No', onPress: () => {}},
-    ]);
+  // const handleDeleteTask = (index) =>{
+  //   Alert.alert('Delete Task', 'Bạn đã hoàn thành việc này?', [
+  //     {
+  //       text: 'Yes',
+  //       onPress: () => {
+  //         let taskListTmp = [...taskList];
+  //         //xoá 1 phần tử từ vị trí index
+  //         taskListTmp.splice(index, 1);
+  //         settaskList(taskListTmp);
+  //       }
+  //     },
+  //     {text: 'No', onPress: () => {}},
+  //   ]);
+  // }
+  const handleDeleteTask = async() =>{
+    try {
+      
+      Alert.alert('Delete Task', 'Bạn đã hoàn thành việc này?', [
+        {
+          text: 'Yes',
+          onPress: () => {
+            id = getAll(TodoListSchema).id;
+            deleteTask(TodoListSchema, id).then().catch(error=>{
+                console.log(error)
+             })
+            // let taskListTmp = [...taskList];
+            // //xoá 1 phần tử từ vị trí index
+            // taskListTmp.splice(index, 1);
+            // settaskList(taskListTmp);
+            
+          }
+        },
+        {text: 'No', onPress: () => {}},
+      ]);
+    } catch (error) {
+        console.log(error)
+    }
   }
   return(
     <View style={styles.container}>
--- database/services.js
+++ database/services.js
@@ -3,6 +3,7 @@ import Realm from "realm";
 
 
 import {databaseOptions} from './allSchema'
+import { TodoListSchema } from "./tables";
 
 export const getAll = (nameTable) => new Promise((resolve, reject) => {
     Realm.open(databaseOptions).then((realm) => {
@@ -22,5 +23,36 @@ export const insert = (nameTable,obj) => new Promise((resolve, reject) => {
         })
     }).catch(e => {
         reject(0)
+    })
+})
+
+export const deleteTask = (nameTable,id) => new Promise((resolve, reject) => {
+    Realm.open(databaseOptions).then((realm)=>{
+        realm.write(()=>{
+            let deleteById = realm.objectForPrimaryKey(nameTable, id);
+            realm.delete(deleteById);
+            resolve(1);
+        })
+    }).catch( e=>{
+        reject(0)
+    })
+})
+
+// export const queryAllTodoList = ()=> new Promise((resolve, reject) => {
+//     Realm.open(databaseOptions).then((realm) => {
+//         let allTodoList = realm.objects(TodoListSchema);
+//         resolve(allTodoList)
+//         resolve(1);
+//     }).catch(e=>{
+//         reject(0)
+//     })
+// })
+export const getId = ()=>new Promise((resolve, reject) => {
+    Realm.open(databaseOptions).then((realm)=>{
+        let all = realm.objects(TodoListSchema);
+        let id = all.id;
+        resolve(id)
+    }).catch(e =>{
+        reject(1)
     })
 })
